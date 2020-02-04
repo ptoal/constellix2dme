532862c94c913482c323ad46d499bdbbd49d0e66
Patrick Toal <ptoal@takeflight.ca>
2020-02-04 00:25:35 -0500

    Basic functionality

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # constellix2dme
+
 Read Constellix export files in json format and create records in DnsMadeEasy
+
+## Background
+
+I leaped before I looked, and moved all my domains to Constellix before realizing that,
+in my case, DME was actually half the price.
+
+This script will read a directory of .json files in the format created by constellix,
+and create Zone files that can be imported into DME.
+
+**NOTE**: I was going to use the DME API to do this, but, from their [API Docs](https://dnsmadeeasy.com/technology/rest-api/):
+
+> Please note: free API calls are limited to 150 calls every five minutes. Additional API calls are priced at $325 for 150 calls every five minutes.
+
+## Usage
+
+./ constellix2dme.py <input_dir> <output_dir>
---
+++ constellix2dme.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# Python script to convert Constellix json files to DME Zone files
+
+import json, os
+import argparse
+
+from jinja2 import Environment, PackageLoader
+env = Environment(
+    loader=PackageLoader('constellix2dme', 'templates'),
+    extensions=['jinja2.ext.do']
+)
+
+def main():
+    # create parser
+    parser = argparse.ArgumentParser()
+    
+    # add arguments to the parser
+    parser.add_argument("--indir", help="Directory containing json zone files from Constellix")
+    parser.add_argument("-i", "--infile", help="Path to a single json zone file from Constellix", action="append")
+    parser.add_argument("--outdir", help="Output directory for Zone files")
+    parser.add_argument("-d", "--debug", action="store_true")
+
+    # parse the arguments
+    args = parser.parse_args()
+
+    debug=args.debug
+
+    if args.indir:
+        indir=args.indir
+    else:
+        indir=os.getcwd()
+
+    if args.outdir:
+        outdir=args.outdir
+    else:
+        outdir=os.getcwd()
+
+    if debug:            
+        import pprint
+        pp = pprint.PrettyPrinter(depth=9)
+
+    if not os.path.isdir(indir):
+        print ("Error: Input Directory doesn't exist.")
+        exit(1)
+
+    if not os.path.isdir(outdir):
+        os.makedirs(outdir)
+        print ("Created directory: ", outdir)
+
+    inputfiles = [f for f in os.listdir(indir) if f.endswith('.' + 'json')]
+    if args.infile:
+        inputfiles.extend(args.infile)
+
+
+    print ("Files to process: " + " ".join(inputfiles))
+
+    for file in inputfiles:
+        handle = open(os.path.join(indir, file),'r')
+        zone_data = json.load(handle)
+
+        if debug:
+            print("ZONE DATA:")
+            pp.pprint(zone_data)
+
+        from jinja2 import Template
+
+        zone_template = env.get_template('zone.j2')
+        zone_context = zone_template.new_context(zone_data)
+        output = zone_template.render(zone_context)
+
+        print (output)
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
+
